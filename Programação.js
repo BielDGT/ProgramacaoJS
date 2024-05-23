da865afe51802c46b409c7e1feb5cb73140befa5
BielDGT <166524378+BielDGT@users.noreply.github.com>
2024-05-23 09:35:09 -0300

    Add files via upload

---
+++ Programação.js
@@ -0,0 +1,30 @@
+//string
+//boolan
+//int
+//float
+
+//var numero1=20
+//var numero2=10
+
+//var soma=numero1+numero2
+//var sub=numero1-numero2
+//var mult=numero1*numero2
+//var div=numero1/numero2
+
+//console.log("Soma dos valores", soma)
+//console.log("Soma dos valores", sub)
+//console.log("Soma dos valores", mult)
+//console.log("Soma dos valores", div)
+
+//TABUADA
+var x=3
+
+console.log(x,"x 1 =",x*1)
+console.log(x,"x 2 =",x*2)
+console.log(x,"x 3 =",x*3)
+console.log(x,"x 4 =",x*4)
+console.log(x,"x 5 =",x*5)
+console.log(x,"x 6 =",x*6)
+console.log(x,"x 7 =",x*7)
+console.log(x,"x 8 =",x*8)
+console.log(x,"x 9 =",x*9)
