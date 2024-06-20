21ae39c143a6d00077d900a5ae3c9abbf4dc43b4
BielDGT <166524378+BielDGT@users.noreply.github.com>
2024-06-20 12:00:11 -0300

    Add files via upload

---
+++ Code Visual/if_codes.js
@@ -0,0 +1,23 @@
+numero = 20
+palavra= "salve"
+
+
+console.log(numero==20)
+/**
+ * Verificar se o numero é mair que 18
+ * Verificar se a palavra é igual a SALVE
+ * Verificar se o numero é posiivo
+ */
+ 
+if (numero > 18) {
+    console.log("O numero é maior que 18")}
+   
+if (palavra = "salve"){
+    console.log("Salve")}
+
+if (numero >0){
+    console.log("É um numero positivo")}
+
+if (numero <0){
+    console.log("É um numero negativo")}
+    
