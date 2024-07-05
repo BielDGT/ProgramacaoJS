a9fa9268de52af1c6fbae2c51a362f0a44dec130
BielDGT <166524378+BielDGT@users.noreply.github.com>
2024-07-05 11:35:20 -0300

    Add files via upload

---
+++ LAL(while).js
@@ -0,0 +1,50 @@
+//1-Imprimir números de 1 a 10
+/*
+i=0
+
+while(i<10){
+    i=i+1
+    console.log(i)
+}
+
+//2-Imprimir números pares de 1 a 20
+
+i=0
+
+while(i<21){
+    i=i+1
+    if (i%2==0){console.log(i)}
+}
+
+//3-Somar números de 1 a 100
+
+i=0
+soma=0
+
+while(i<100){
+    i=i+1
+    console.log(soma=soma+i)
+}
+
+//4-Tabuada do 5
+
+    i=0
+
+    while(i<10){
+        i=i+1
+        console.log("5 X",i,"=",i*5)
+}
+
+//5-Imprimir uma frase 10 vezes
+
+i=0
+
+while(i<10){
+    i=i+1
+    console.log("Eu estou aprendendo laços de repetição")
+*/
+//6-Fatorial de um número
+
+    i=0
+    
+    while()
