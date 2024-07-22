8926eed3c32f5eee212008a842365d431fe01a13
BielDGT <166524378+BielDGT@users.noreply.github.com>
2024-07-21 21:01:48 -0300

    Add files via upload

--- LAL(while).js
+++ LAL(while).js
@@ -42,9 +42,53 @@ i=0
 while(i<10){
     i=i+1
     console.log("Eu estou aprendendo laços de repetição")
-*/
+}
+
+
 //6-Fatorial de um número
 
-    i=0
-    
-    while()
+n=8
+i=0
+fatorial=1
+
+while(i!=n){
+    fatorial=fatorial*n
+    n=n-1
+}
+console.log(fatorial)
+
+
+//7-Imprima os primeiros 10 números da sequência de Fibonacci utilizando um laço de repetição
+
+n=10
+i=1
+numero1=0
+numero2=1
+Fibonacci=0
+
+while(i!=n){
+    Fibonacci=numero1+numero2
+    numero1=numero2
+    numero2=Fibonacci
+    n=n-1
+    console.log(Fibonacci)
+}
+
+//8-Imprima todos os números primos de 1 a 50 utilizando um laço de repetição
+
+numeroF=1
+
+while(numeroF<=50){
+    if(numeroF==2 || numeroF==3||numeroF==5||numeroF==7||numeroF%1==0 && numeroF%numeroF==0 && numeroF%2!=0 && numeroF%3!=0 && numeroF%5!=0 && numeroF%7!=0){
+        console.log(numeroF)
+    }
+    numeroF=numeroF+1
+}*/
+//10- Implemente uma contagem regressiva de 10 até 0 utilizando um laço de repetição.
+
+i=10
+
+while(i>-1){
+    console.log(i)
+    i=i-1
+}
